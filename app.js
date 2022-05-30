7d1a64857147b5987e2ff1ab08c4c06de7708b84
Kelechi Nwosu <kjpn00@gmail.com>
2022-05-29 22:23:30 -0600

    added bootstrao

--- app.js
+++ app.js
@@ -8,18 +8,13 @@ form.addEventListener('submit', async function (e) {
     const playerData = await (res.data.data[0]);
     let firstName = (res.data.data[0].first_name);
     let lastName = (res.data.data[0].last_name)
+    let weight = `${(res.data.data[0].weight_pounds)} lbs`
     const height = `${(res.data.data[0].height_feet)} ft ${(res.data.data[0].height_inches)} in`
     const heading = document.createElement('h2');
     const team = (res.data.data[0].team.full_name);
     heading.innerHTML = `${firstName} ${lastName} - ${team}`;
-    const playerDetails = document.createElement('p')
-    playerDetails.innerHTML = height
-
-    const searchBar = document.querySelector('#searchBar')
-    searchBar.addEventListener('click', () => {
-        searchBar.innerHTML = ''
-    })
-
+    const playerDetails = document.createElement('h3')
+    playerDetails.innerHTML = `${height} ${weight}`
 
 
     document.body.append(heading)
